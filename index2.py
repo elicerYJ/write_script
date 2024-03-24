892f673b7bc0c581e5b238589ba4bf4fd76e3dfc
elicerYJ <128034312+elicerYJ@users.noreply.github.com>
2024-03-24 23:35:50 +0900

    first commit

---
+++ index2.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+import openpyxl  # Excel 파일로 저장하기 위해 필요
+from io import BytesIO  # 파일 다운로드를 위해 필요
+
+# 데이터프레임을 저장할 빈 데이터프레임 생성
+if 'df' not in st.session_state:
+    st.session_state.df = pd.DataFrame(columns=['페이지 번호', '애니메이션 적용 대상', '사용할 대본'])
+
+course_name = st.text_input("강의명", "도레미파이썬")
+
+# 사용자 입력 받기
+with st.form(key='record_form'):
+    page_number = st.number_input("페이지 번호", value=None, step=1)
+    
+    animation_target = st.multiselect(
+        '애니메이션 적용 대상',
+        ['⛔ 없음', "🔠 텍스트", '🆚 도형을 포함한 텍스트', '🟪 도형', '⚠️ 아이콘', '🖼️ 이미지 또는 코드', '✨ 효과', '🎸 기타']
+        )
+    
+    script = st.text_area('사용할 대본', height = 400)
+    
+    submit_button = st.form_submit_button(label='스크립트 추가')
+
+# 정보 추가 버튼이 클릭되었을 때
+if submit_button:
+    # 입력받은 정보를 데이터프레임에 추가
+    new_data = {'페이지 번호': page_number, '애니메이션 적용 대상': animation_target, '사용할 대본': script}
+    st.session_state.df = pd.concat([st.session_state.df, pd.DataFrame([new_data])], ignore_index=True)
+
+# 데이터프레임을 화면에 표시
+st.checkbox("Use container width", value=False, key="use_container_width")
+
+st.dataframe(
+    st.session_state.df, 
+    use_container_width=st.session_state.use_container_width,
+    hide_index=True
+    )
+
+# 데이터프레임을 Excel 파일로 변환하는 함수
+def to_excel(df):
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+    # 파일 데이터를 가져와서 반환
+    return output.getvalue()
+
+
+if st.button('다운로드'):
+    val = to_excel(st.session_state.df)
+    st.download_button(label='현재 데이터 다운로드', data=val, file_name=f"{course_name}_script.xlsx", mime='application/vnd.ms-excel')
+
+st.divider()
+
+# 데이터프레임 초기화 버튼
+if st.button('스크립트 초기화'):
+    st.session_state.df = pd.DataFrame(columns=['페이지 번호', '애니메이션 적용 대상', '사용할 대본'])
